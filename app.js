fca461202066d97b2b6c3e1c282ca6f293c129be
Nick Maclaren <nt.maclaren@gmail.com>
2024-01-06 13:09:44 +0000

    Primed app.js for express-session, just need to implement. Changed port of app.js. Added rate limiting to nginx config files. Trying to use pm2 to auto-reload app.js when I make dev changes, but not sure if it's working properly. Removed nodemon. Etc.

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-// /home/nick/projects/theGameHub.Express/app.js
+// /var/www/html/bytebloom.tech/express/app.js
 
-const bcrypt = require('bcrypt');
-const express = require('express');
-const bodyParser = require('body-parser');
 require("dotenv").config();
+const express = require('express');
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
 const stripe = require('stripe')("process.env.TEST_SECRET_KEY");
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
 
 const app = express();
-const port = 3000;
+const port = 7321;
 
 // Middleware
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static("public"));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+}));
 
 // Temporary array stand-in for database
 const users = [];
@@ -20,7 +28,7 @@ const users = [];
 // Routes
 app.post("/signupRequest", function(req, res) {
     const requestBody = req.body;
-    // Left off here, need to implement a database
+    // Left off here, need to implement a database (kill me)
 });                                                
 
 app.post("/loginRequest", function(req, res) {
@@ -28,6 +36,10 @@ app.post("/loginRequest", function(req, res) {
 });
 
 app.post("/create-checkout-session", function(req, res) {
+    // Implement
+});
+
+app.get("*", function(req, res) {
     // Implement
 });
 
